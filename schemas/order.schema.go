0cc8866d0daaa3681d45719ebf5ab8ae79166a94
Hussain Ali <hussainalee@icloud.com>
2026-02-13 01:16:49 +0500

    Enhance CreateOrderRequest and Item schemas with improved validation and binding rules

--- schemas/order.schema.go
+++ schemas/order.schema.go
@@ -13,23 +13,23 @@ type OrderStatus struct {
 }
 
 type Item struct {
-	ProductID string  `bson:"product_id" json:"product_id,omitempty" binding:"required"`
+	ProductID string  `bson:"product_id" json:"product_id,omitempty" binding:"uuid4"`
 	Quantity  int64   `bson:"quantity" json:"quantity,omitempty" binding:"required"`
 	Price     float64 `bson:"price" json:"price,omitempty" binding:"required"`
 }
 
 type CreateOrderRequest struct {
 	Id                 bson.ObjectID            `bson:"_id,omitempty" json:"-"`
-	OrderID            string                   `bson:"order_id,index" json:"order_id,omitempty"`
-	UserID             string                   `bson:"user_id" json:"user_id,omitempty"`
-	ShipAddressID      string                   `bson:"ship_address_id" json:"ship_address_id,omitempty"`
+	OrderID            string                   `bson:"order_id,index" json:"order_id,omitempty" binding:"omitempty,uuid4"`
+	UserID             string                   `bson:"user_id" json:"user_id,omitempty" binding:"omitempty,uuid4"`
+	ShipAddressID      string                   `bson:"ship_address_id" json:"ship_address_id,omitempty" binding:"omitempty,uuid4"`
 	Items              []Item                   `bson:"items" json:"items" binding:"required,min=1,dive"`
 	DiscountAmount     *float64                 `bson:"discount_amount" json:"discount_amount,omitempty"`
 	DiscountCode       string                   `bson:"discount_code" json:"discount_code,omitempty"`
-	TotalPrice         *float64                 `bson:"total_price" json:"total_price" binding:"required_if=,min=0"`
-	VAT                *float64                 `bson:"vat" json:"vat,omitempty" binding:"required,min=0"`
+	TotalPrice         float64                  `bson:"total_price" json:"total_price" binding:"required_if_item_is_available"`
+	VAT                float64                  `bson:"vat" json:"vat,omitempty" binding:"required_if_item_is_available"`
 	PaymentMethod      *constants.PaymentMethod `bson:"payment_method" json:"payment_method,omitempty"`
-	PaymentReferenceID string                   `bson:"payment_reference_id" json:"payment_reference_id,omitempty"`
+	PaymentReferenceID string                   `bson:"payment_reference_id" json:"payment_reference_id,omitempty" binding:"omitempty,uuid4"`
 	Status             *OrderStatus             `bson:"status" json:"status,omitempty"`
 	OrderedAt          time.Time                `bson:"ordered_at" json:"ordered_at"`
 }
