3cdfcb6e286ecbefda0a8418550aa497642d9b84
Hussain Ali <hussainalee@icloud.com>
2026-02-13 01:16:34 +0500

    Fix typos in PaymentStatus constants and improve code formatting

--- constants/order.go
+++ constants/order.go
@@ -4,12 +4,20 @@ type PaymentMethod string
 type PaymemtStatus string
 
 const (
+	// payment method
 	ORDER_CREDIT_CARD PaymentMethod = "credit_card"
 	ORDER_DOC         PaymentMethod = "cod"
-)
 
-const (
+	// payment status
 	ORDER_PENDING   PaymemtStatus = "pedning"
 	ORDER_COMPLETED PaymemtStatus = "completed"
 	ORDER_REJECT    PaymemtStatus = "reject"
+
+	//
+
 )
+
+// work with iota
+// func (ps PaymemtStatus) String() string {
+// 	return [...]string{"pending", "completed", "reject"}[ps]
+// }
