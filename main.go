a914cae0de196233ee38d89ac8f22f47355c3418
Hussain Ali <hussainalee@icloud.com>
2026-02-13 01:26:56 +0500

    Add custom request validation and enhance CORS middleware for JSON header checks

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hs622/ecommerce-cart/middleware"
 	"github.com/hs622/ecommerce-cart/routes"
 	"github.com/hs622/ecommerce-cart/utils"
+	"github.com/hs622/ecommerce-cart/utils/validation"
 )
 
 type variables struct {
@@ -55,6 +56,9 @@ func main() {
 	}
 	defer mongodb.Disconnect()
 
+	// Custom request validation registration
+	validation.RegisterCustomRequestValidation()
+
 	// initialising repositories
 	gin.SetMode(gin.ReleaseMode)
 
@@ -66,6 +70,7 @@ func main() {
 	http.Use(gin.Logger())
 	http.Use(gin.Recovery())
 	http.Use(middleware.CORSMiddleware())
+	http.Use(middleware.POSTRequestMiddleware())
 	// http.Use(middleware.Authentication())
 	api := http.Group("/api")
 
