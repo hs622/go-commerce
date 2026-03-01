b726451071ac5676e2a272d2596c7d60d6cb7a6c
Hussain Ali <hussainalee@icloud.com>
2026-03-01 18:27:18 +0500

    Update .gitignore to exclude utils/validation.go and refactor regex declarations in ToSnakeCase function

--- utils/subfunc.go
+++ utils/subfunc.go
@@ -26,10 +26,11 @@ func StringFuncName(skip int) string {
 	return f.Name()
 }
 
-var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
-
 func ToSnakeCase(str string) string {
+
+	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
+
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(str, "${1}_${2}")
 	return strings.ToLower(snake)
