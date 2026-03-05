39ae7c078e736ed512b7757ef01e182d541c31e7
Hussain Ali <hussainalee@icloud.com>
2026-03-05 18:11:55 +0500

    Implement user update and retrieval functionalities in UserHandler

--- handlers/user.handler.go
+++ handlers/user.handler.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hs622/ecommerce-cart/repository"
 	"github.com/hs622/ecommerce-cart/schemas"
 	"github.com/hs622/ecommerce-cart/utils"
+	"github.com/hs622/ecommerce-cart/utils/database"
 )
 
 type UserHandler struct {
@@ -57,11 +58,84 @@ func (r *UserHandler) CreateUser(ctx *gin.Context) {
 		return
 	}
 
-	utils.SuccessResponse(ctx, http.StatusOK, "user created successfully.", &user)
+	utils.SuccessResponse(ctx, http.StatusCreated, "user created successfully.", &user)
 }
 
-func (r *UserHandler) UpdateUser(ctx *gin.Context)  {}
-func (r *UserHandler) SuspendUser(ctx *gin.Context) {}
-func (r *UserHandler) DeleteUser(ctx *gin.Context)  {}
-func (r *UserHandler) GetUser(ctx *gin.Context)     {}
-func (r *UserHandler) GetUsers(ctx *gin.Context)    {}
+func (r *UserHandler) UpdateUser(ctx *gin.Context) {
+
+	userId := ctx.Param("userId")
+	requestError := make(map[string]string)
+	var fields schemas.PatchUserRequest
+	var user schemas.CreateUserRequest
+
+	if !utils.CheckUuid(userId) {
+		requestError["error"] = "invalid user id."
+		utils.ErrorResponse(ctx, http.StatusBadRequest, "Invalid resource id.", requestError)
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&fields); err != nil {
+
+		var unMarshalErr *json.UnmarshalTypeError
+		if errors.As(err, &unMarshalErr) {
+			requestError[strings.ToLower(unMarshalErr.Field)] = fmt.Sprintf(
+				"field %s must be a %s",
+				unMarshalErr.Field,
+				unMarshalErr.Type.String(),
+			)
+		}
+
+		var validate validator.ValidationErrors
+		if errors.As(err, &validate) {
+			for _, f := range err.(validator.ValidationErrors) {
+				requestError[strings.ToLower(f.Field())] = strings.ToLower(f.Tag())
+			}
+		}
+
+		utils.ErrorResponse(ctx, http.StatusBadRequest, "Invalid request payload.", requestError)
+		return
+	}
+
+	if err := r.repo.UpdateUser(ctx.Request.Context(), userId, &fields, &user); err != nil {
+		requestError["error"] = err.Error()
+		utils.ErrorResponse(ctx, http.StatusBadRequest, "Unable to update the field.", requestError)
+		return
+	}
+
+	utils.SuccessResponse(ctx, http.StatusAccepted, "User update successfully.", &user)
+}
+
+func (r *UserHandler) GetUser(ctx *gin.Context) {
+
+	var user schemas.CreateUserRequest
+	requestErrors := make(map[string]string)
+
+	if err := r.repo.FetchUser(
+		ctx,
+		&user,
+		database.UserFetchOptions{
+			Url:      ctx.Request.URL,
+			FullPath: ctx.FullPath(),
+		}); err != nil {
+		fmt.Println(err)
+		requestErrors["error"] = err.Error()
+		utils.ErrorResponse(ctx, http.StatusBadRequest, "Invalid request", requestErrors)
+		return
+	}
+
+	utils.SuccessResponse(ctx, http.StatusOK, "Fetch user successfully.", &user)
+}
+
+func (r *UserHandler) GetUsers(ctx *gin.Context) {
+
+	var users []schemas.CreateUserRequest
+	requestErrors := make(map[string]string)
+
+	if err := r.repo.FetchUsers(ctx, ctx.Request.URL, &users); err != nil {
+		requestErrors["error"] = err.Error()
+		utils.ErrorResponse(ctx, http.StatusBadRequest, "", requestErrors)
+		return
+	}
+
+	utils.SuccessResponse(ctx, http.StatusOK, "fetch user successful.", users)
+}
